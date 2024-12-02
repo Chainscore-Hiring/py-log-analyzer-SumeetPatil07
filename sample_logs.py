ccac9f5c995798c727a2c11c0477fd91dd9321a9
SumeetPatil07 <psumeet334@gmail.com>
2024-12-02 20:01:41 +0530

    Create sample_logs.py

---
+++ sample_logs.py
@@ -0,0 +1,6 @@
+# sample_logs.py
+SAMPLE_LOGS = """
+2024-01-24 10:15:32.123 INFO Request processed in 127ms
+2024-01-24 10:15:33.001 ERROR Database connection failed
+2024-01-24 10:15:34.042 INFO Request processed in 95ms
+"""
